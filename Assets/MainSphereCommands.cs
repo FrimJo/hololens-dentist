e18f544ad4ea377c1313eb0af9a5b022664bfb31
davidbergvik <davidbergvik@gmail.com>
2016-12-15 17:05:02 +0100

    ADD looping showmenu animation from start of runtime.

---
+++ Assets/MainSphereCommands.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSphereCommands : MonoBehaviour
+{
+    Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnSelect()
+    {
+        anim.SetBool("isOpen", true);
+        Debug.Log(anim.GetBool("isOpen"));
+    }
+}
