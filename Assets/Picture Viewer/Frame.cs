34203eaa1eee49eac939fdcf78437e5bb6123db0
christianfischer <christian@chfi.se>
2016-12-16 14:14:29 +0100

    Add X-ray viewer

---
+++ Assets/Picture Viewer/Frame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame : MonoBehaviour {
+
+    private const int numPictures = 5;
+    public Texture[] pictures = new Texture[numPictures];
+    private int curPicture = 0;
+
+    private Material material;
+
+    private MeshRenderer meshRenderer;
+
+    // Use this for initialization
+    void Start () {
+        material = GetComponent<Renderer>().material;
+        updatePicture();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    int normalizeIndex(int index)
+    {
+        return (index + pictures.Length) % pictures.Length;
+    }
+
+    void updatePicture()
+    {
+        material.mainTexture = pictures[curPicture];
+    }
+
+    void NextPicture()
+    {
+        curPicture = normalizeIndex(curPicture + 1);
+        updatePicture();
+    }
+
+    void PrevPicture()
+    {
+        curPicture = normalizeIndex(curPicture - 1);
+        updatePicture();
+    }
+}
