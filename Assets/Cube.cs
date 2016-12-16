310b550aed5602be97a299c26e0ce009fff5d2c0
Christian Fischer <christian@chfi.se>
2016-12-16 12:26:29 +0100

    a moveable cube

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 using HoloToolkit.Unity.InputModule;
 
-public class Cube : MonoBehaviour, IManipulationHandler, IFocusable
+public class Cube : MonoBehaviour, IManipulationHandler, IFocusable, IInputHandler
 {
 
     private MeshRenderer meshRenderer;
@@ -17,31 +17,38 @@ public class Cube : MonoBehaviour, IManipulationHandler, IFocusable
 
     private Color prevColor;
 
+	private bool isHeld;
+	private Vector3 startPos;
+
+	private IInputSource currentInputSource;
+	private uint currentInputSourceId;
+
     // Use this for initialization
     void Start ()
 	{
 	    meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
         material = GetComponent<Renderer>().material;
         originalColor = material.color;
+		isHeld = false;
     }
 
     // Update is called once per frame
     void Update ()
 	{
-        /*
-	    var headPos = Camera.main.transform.position;
-	    var gazeDir = Camera.main.transform.forward;
-	    RaycastHit hitInfo;
+		if (isHeld) {
+			Vector3 curHandPos;
+			currentInputSource.TryGetPosition(currentInputSourceId, out curHandPos);
 
-	    if (Physics.Raycast(headPos, gazeDir, out hitInfo))
-	    {
-	        meshRenderer.enabled = true;
-	    }
-	    else
-	    {
-	        meshRenderer.enabled = false;
-	    }
-        */
+			print(curHandPos);
+			//transform.position += ((curHandPos - startPos) * 0.01F);
+			print(curHandPos - startPos);
+
+			//transform.LookAt(Camera.main.transform);
+
+			//transform.Rotate(Vector3(0f,Vector3.Angle(startPos, curHandPos),0f));
+			//transform.forward = Camera.main.transform.InverseTransformVector(Camera.main.transform.forward * -1.0F);
+			//transform.forward curHandPos - startPos;
+		}
 	}
 
     public void OnFocusEnter()
@@ -53,6 +60,33 @@ public class Cube : MonoBehaviour, IManipulationHandler, IFocusable
     {
         material.color = originalColor;
     }
+
+	public void OnInputDown(InputEventData eventData)
+	{
+		//if (!eventData.InputSource.GetSupportedInputInfo(eventData.SourceId, SupportedInputInfo.Position))
+		if (!eventData.InputSource.SupportsInputInfo(eventData.SourceId, SupportedInputInfo.Position))
+			return;
+
+		currentInputSource = eventData.InputSource;
+		currentInputSourceId = eventData.SourceId;
+		isHeld = true;
+
+		Vector3 gazeHitPos = GazeManager.Instance.HitInfo.point;
+		//	Vector3 handPosition;
+		currentInputSource.TryGetPosition(currentInputSourceId, out startPos);
+		//startPos = Camera.main.transform.InverseTransformDirection(transform - gazeHitPos);
+		material.color = SelectedColor;
+	}
+
+	public void OnInputUp(InputEventData eventData)
+	{
+		if (currentInputSource != null &&
+		    eventData.SourceId == currentInputSourceId) {
+			
+			material.color = originalColor;
+			isHeld = false;
+		}
+	}
 
     public void OnManipulationStarted(ManipulationEventData eventData)
     {
