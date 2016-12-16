005cb754b62bdcc110e95e7cea4440a59b86f57a
davidbergvik <davidbergvik@gmail.com>
2016-12-16 14:28:52 +0100

    ADD 10000 files but holotoolkit still dont with with select

--- Assets/MainSphereCommands.cs
+++ Assets/MainSphereCommands.cs
@@ -1,25 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MainSphereCommands : MonoBehaviour
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.Events;
+public class MainSphereCommands : MonoBehaviour, IInputClickHandler, IFocusable
 {
     Animator anim;
+    bool isOpen;
+    bool isFocused;
     // Use this for initialization
     void Start()
     {
-        anim = GetComponent<Animator>();
-
+        anim = this.GetComponentInParent<Animator>();
+        this.isOpen = false;
+        this.isFocused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (this.isFocused)
+        {
+            anim.SetBool("menuFocused", true);
+        }
     }
-    void OnSelect()
+    public void OnFocusEnter()
+      {
+        this.isFocused = true;
+      }
+
+      public void OnFocusExit()
+      {
+        this.isFocused = false;
+    }
+  
+    public void OnInputClicked(InputEventData eventData)
     {
         anim.SetBool("isOpen", true);
-        Debug.Log(anim.GetBool("isOpen"));
+        if (anim.GetBool("isOpen"))
+        {
+            anim.SetBool("isOpen", false);
+        }
+        else
+        {
+            anim.SetBool("isOpen", true);
+        }
+
     }
+    
 }
