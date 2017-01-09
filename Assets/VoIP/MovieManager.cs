80b487c4039ca268ae705fc1a7f2152e5c31ba71
Fredrik Johansson <c04fjn@cs.umu.se>
2017-01-09 10:03:37 +0100

    Added VoIP
    Added VoIP to DentistFunctions

---
+++ Assets/VoIP/MovieManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class MovieManager : MonoBehaviour, IInputClickHandler {
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	public void OnInputClicked(InputEventData eventData) {
+		( (MovieTexture) GetComponent<Renderer>().material.mainTexture ).Play();
+	}
+}
