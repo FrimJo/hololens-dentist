8ba56b046fb9b281e74fe4e50c41fd70beb496e0
mattiasedin <mattias.edin92@gmail.com>
2017-01-27 13:52:10 +0100

    added thickness to panels

---
+++ Assets/Journal/Scripts/PanelColorManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelColorManager : MonoBehaviour {
+
+    // Use this for initialization
+    public Color ActiveColor;
+    public Color InactiveColor;
+
+    private bool isActive = true;
+
+    public bool IsActive
+    {
+        get { return this.isActive; }
+        set
+        {
+            isActive = value;
+            this.GetComponent<MeshRenderer>().material.color = (value ? ActiveColor : InactiveColor);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        IsActive = isActive;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Toggle()
+    {
+        IsActive = !isActive;
+    }
+}
