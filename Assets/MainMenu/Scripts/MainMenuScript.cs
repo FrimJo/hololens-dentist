fd7c1a274ca2ac2bf2094d4012ffee600340e9b8
Fredrik Johansson <c04fjn@cs.umu.se>
2017-02-21 14:44:04 +0100

    script for controlling the main menu, now it only contains script for unlocking the menu at start

---
+++ Assets/MainMenu/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+	void Start () {
+
+		// Set status to unlocked
+		DentistItemScript _parentScript = GetComponentInParent<DentistItemScript> ();
+
+		// If parent has script
+		if (_parentScript) {
+
+			// Set status to placing
+			_parentScript.ChangeStatus (DentistItemScript.Statuses.Placing);
+		}
+
+	}
+
+}
