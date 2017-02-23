705bdc42134bd10197e0834f280229d00c6c2fe2
Fredrik Johansson <c04fjn@cs.umu.se>
2017-02-23 20:13:40 +0100

    added code to the button to play and stop sound

--- Assets/VoIP/CallButtonManager.cs
+++ Assets/VoIP/CallButtonManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using HoloToolkit.Unity.InputModule;
+﻿using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,10 +12,11 @@ public class CallButtonManager : MonoBehaviour, IInputClickHandler {
 	public GameObject toPlay;
 
 	private MovieTexture _movie;
+	private AudioSource _audio;
 
 	void Start () {
-		_movie = (MovieTexture) this.toPlay.GetComponent<RawImage> ().mainTexture;
-
+		_movie = (MovieTexture) toPlay.GetComponent<RawImage> ().mainTexture;
+		_audio = GetComponent<AudioSource>();
 
 
 		//_movie = (MovieTexture)this.toPlay.GetComponent<Image> ().material.mainTexture;
@@ -34,13 +33,14 @@ public class CallButtonManager : MonoBehaviour, IInputClickHandler {
 	public void OnInputClicked(InputEventData eventData)
 	{
 		
-		if (this._movie.isPlaying)
+		if (_movie.isPlaying)
 		{
 			// Stop movie
-			this._movie.Stop ();
+			_movie.Stop ();
+			_audio.Stop();
 
 			// Change color
-			GetComponent<Renderer> ().material = this.playMaterial;
+			GetComponent<Renderer> ().material = playMaterial;
 
 			// Move button inwards
 			transform.localPosition -= Vector3.right * 0.0326f;
@@ -48,10 +48,11 @@ public class CallButtonManager : MonoBehaviour, IInputClickHandler {
 		else
 		{
 			// Play movie
-			this._movie.Play ();
+			_movie.Play ();
+			_audio.Play();
 
 			// Change color
-			GetComponent<Renderer> ().material = this.stopMaterial;
+			GetComponent<Renderer> ().material = stopMaterial;
 
 			// Move button outwards
 			transform.localPosition += Vector3.right * 0.0326f;
