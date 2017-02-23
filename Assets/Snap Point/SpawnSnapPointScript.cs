7704e508ec963d01875cddd3227e671383965d2d
DESKTOP-SC0MCNH\admin <admin@thanx.se>
2017-02-23 23:34:51 +0100

    added support for spawning using the menu

---
+++ Assets/Snap Point/SpawnSnapPointScript.cs
@@ -0,0 +1,18 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class SpawnSnapPointScript : MonoBehaviour, IInputClickHandler {
+
+    public GameObject snapPointPrefab;
+
+    void Start () { }
+
+	void Update () { }
+
+    public void OnInputClicked(InputEventData eventData)
+    {
+        Vector3 spawnPoint = Vector3.Lerp(Camera.main.transform.position, transform.position, 0.5f);
+        GameObject snapPpoint = Instantiate(snapPointPrefab, spawnPoint, Quaternion.identity);
+        DentistItemScript script = snapPointPrefab.GetComponent<DentistItemScript>();
+    }
+}
